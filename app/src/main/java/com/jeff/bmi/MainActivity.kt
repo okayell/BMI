90e7715d9c321b32de9699a9fbb2aa31c7293c7e
Jeff <okayell@hotmail.com>
2021-10-14 21:11:29 +0800

    linearlayout and bmi button

--- app/src/main/java/com/jeff/bmi/MainActivity.kt
+++ app/src/main/java/com/jeff/bmi/MainActivity.kt
@@ -2,10 +2,24 @@ package com.jeff.bmi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.jeff.bmi.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+    fun bmi(view: View) {
+        val weight = ed_weight.text.toString().toFloat()
+        val height = ed_height.text.toString().toFloat()
+        val bmi = weight / (height * height)
+        Log.d("BMI", bmi.toString())
     }
 }
